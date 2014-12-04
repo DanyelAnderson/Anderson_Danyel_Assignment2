9cb5b133b0b86af47cc1021675e73babfbd27b8c
DanyelAnderson <Danyel@Danyels-MacBook-Pro.local>
2014-12-04 11:11:37 -0500

    finally working
    start of assignment 2. without fixes

---
+++ Anderson_Danyel_Assignment2.js
@@ -0,0 +1,53 @@
+//alert("JavaScript works!");
+
+//Danyel Anderson
+//SDI 1412
+//Project 2
+
+
+//My Variables
+var dishwasherFull = true;
+var howLongDishesWash = 30;
+var dishwasherLocation = "kitchen";
+var numberPotsAndPans = " 2 pots and pans";
+var dirty = "yes";
+var notDirty = "no";
+
+
+console.log ( " Time to do the dishes? We need to check the sink for dirty pots and pans though. " )
+
+
+//Prompt
+potsAndPans = prompt( " Are the pots and pans " + dirty + " or " + notDirty + " ? " );
+
+
+//Procedure
+  if ( potsAndPans === "dirty" ) {
+	  //run if pots and pans are dirty
+	  console.log( "Pots and pans are dirty.")
+
+	
+//confirm
+areYourSure = confirm ( " Are you sure they are dirty? ")
+
+//Boolean Function
+  if ( yesTheyAre === true ) {
+     // run if confirm is true
+	 function howManyPotsAndPans() {
+		 console.log(" You have 2 pots and pans to wash.")
+		 var numPotsAndPans = 2;
+		 //While loop
+		   while ( numberOfPotsAndPans = 0 ) {
+		   numberOfPotsAndPans ++
+		   console.log ( numberOfPotsAndPans)
+		   if ( numberOfPotsAndPans < 1) {
+			   console.log ( " We have pots and pans to wash first!")
+		   }
+	    }
+	 }potsAndPansTotal()
+  }
+  } if (
+		 
+		 
+
+	 
