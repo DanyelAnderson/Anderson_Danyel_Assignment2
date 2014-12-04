3e479d3f8fa7d24f4bfbe480c70aaa3906d9f64e
DanyelAnderson <Danyel@Danyels-MacBook-Pro.local>
2014-12-04 11:56:23 -0500

    redo
    redid original file

--- Anderson_Danyel_Assignment2.js
+++ Anderson_Danyel_Assignment2.js
@@ -1,6 +1,6 @@
-//alert("JavaScript works!");
+alert("JavaScript works!");
 
-//Danyel Anderson
+/Danyel Anderson
 //SDI 1412
 //Project 2
 
@@ -10,44 +10,47 @@ var dishwasherFull = true;
 var howLongDishesWash = 30;
 var dishwasherLocation = "kitchen";
 var numberPotsAndPans = " 2 pots and pans";
-var dirty = "yes";
+var notClean = "yes";
 var notDirty = "no";
+var soak = "We need to soak these dirty pots and pans.";
 
 
 console.log ( " Time to do the dishes? We need to check the sink for dirty pots and pans though. " )
 
 
 //Prompt
-potsAndPans = prompt( " Are the pots and pans " + dirty + " or " + notDirty + " ? " );
+potsAndPans = prompt( " Are the pots and pans " + notClean + " or " + notDirty + " ? " );
 
 
 //Procedure
-  if ( potsAndPans === "dirty" ) {
+  if ( potsAndPans === "notClean" ) {
 	  //run if pots and pans are dirty
 	  console.log( "Pots and pans are dirty.")
+  }
+     else {
+		 console.log ( " We do not have any dirty pots and pans! ")
+	 }
 
 	
 //confirm
 areYourSure = confirm ( " Are you sure they are dirty? ")
 
 //Boolean Function
-  if ( yesTheyAre === true ) {
-     // run if confirm is true
-	 function howManyPotsAndPans() {
+
+var soak  = function ( soak ) {
+  if ( yesTheyAre ==true )
+       var dirty = false
+  }
+  else {
+	  var dirty = true 
+  }
+} 
 		 console.log(" You have 2 pots and pans to wash.")
 		 var numPotsAndPans = 2;
 		 //While loop
-		   while ( numberOfPotsAndPans = 0 ) {
-		   numberOfPotsAndPans ++
-		   console.log ( numberOfPotsAndPans)
-		   if ( numberOfPotsAndPans < 1) {
-			   console.log ( " We have pots and pans to wash first!")
-		   }
-	    }
-	 }potsAndPansTotal()
-  }
-  } if (
-		 
+		 while (numPotsAndPans = 0 ) {
+			 console.log ( " We have " + numPotsAndPans + " to wash.");
+			 
 		 
 
 	 
