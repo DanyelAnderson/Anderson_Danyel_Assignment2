0e79aa4c35924afb470b06273d9d46313ee3ad35
DanyelAnderson <Danyel@Danyels-MacBook-Pro.local>
2014-12-04 21:37:55 -0500

    finally
    all finished up

--- Anderson_Danyel_Assignment2.js
+++ Anderson_Danyel_Assignment2.js
@@ -13,13 +13,17 @@ var numberPotsAndPans = " 2 pots and pans";
 var notClean = "yes";
 var notDirty = "no";
 var soak = "We need to soak these dirty pots and pans.";
+var p1 = "There are dirty dishes";
+var q1 = "There are clean dishes";
+var p = true, q = false;
 
 
-console.log ( " Time to do the dishes? We need to check the sink for dirty pots and pans though. " )
+
+console.log ( " Time to do the dishes? We need to check the sink for dirty pots and pans first. " )
 
 
 //Prompt
-potsAndPans = prompt( " Are the pots and pans " + notClean + " or " + notDirty + " ? " );
+potsAndPans = prompt( " Are the pots and pans dirty " + notClean + " or " + notDirty + " ? " );
 
 
 //Procedure
@@ -28,23 +32,24 @@ potsAndPans = prompt( " Are the pots and pans " + notClean + " or " + notDirty +
 	  console.log( "Pots and pans are dirty.")
   }
      else {
+		 (potsAndPans === "clean") 
 		 console.log ( " We do not have any dirty pots and pans! ")
 	 }
 
 	
 //confirm
-areYourSure = confirm ( " Are you sure they are dirty? ")
+areYourSure = confirm ( " Are you ready for the next step, press ok to continue or cancel to cancel to end.")
 
 //Boolean Function
-
-var soak  = function ( soak ) {
-  if ( yesTheyAre == true ){
-       var dirty = false
+// Given (p ^ q )
+if ( p && q ) {
+  console.log ( " The expression is true ");
+  
+  } else {
+	  console.log ( " The expression is false ");
+ 
   }
-  else {
-	  var dirty = true 
-  }
-}
+{}
           console.log(" It looks like we do not have to worry about unloading the dishwasher, thank goodness.")
 		 var numPotsAndPans = 0;
 		 //While loop
@@ -53,25 +58,25 @@ var soak  = function ( soak ) {
 			 console.log ( " We can now move on to the next dishwashing step.")
 			 
 //Number Function
-		function timeToDoDishes ( regular, potsAnPans) {
+		function timeToDoDishes ( dishesWash, potsAnPans) {
 			var regularDishes = 12
 			var potsAnPans = 15
 			var totalTimeAll = regular + potsAnPans
 		}
-regularDishes = 12
+dishesWash = 30
 potsAnPans = 15
-var totalTime = regularDishes + potsAnPans	
-for (var dishwasherTime = 32; dishwasherTime <= 60 ; dishwasherTime += totalTime) {
-	console.log (" It will take you " + totalTime + " minutes to complete your task. ") };
+var totalTime = dishesWash + potsAnPans	
+{
+	console.log (" It will take you " + totalTime + " minutes to complete your task of loading dishes into the dishwasher.") };
 	
 
 //String Function
-var finished = confirm("Finally all the dishes are done.")
-var next = confirm (" We must put away the clean dishes, so we can skip that step next time.")
-var relax = confirm (" Now we can relax after doing our chore!")
-console.log ( finished + next + relax )
+var finished = ("Finally all the dishes are done.")
+var next = (" We must put away the clean dishes, so we can skip that step next time.")
+var relax = (" Now we can relax after doing our chore!")
+console.log( finished + next + relax )
 
 //Final Output
 
-console.log ( " Dish chore is finally done, it is a easy task but yet something most people still do not care to do. Now to relax a little until the next chore is to be done, Folding Laundry! " )
+console.log ( " Dish chore is finally done, it is a easy task but yet something most people still do not care to do. Now to relax a little until the next chore is to be done, folding the laundry! " )
 	
